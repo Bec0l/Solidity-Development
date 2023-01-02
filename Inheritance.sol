87c8ac0a87e0d817588e567cd7db48c18d567091
Bec0l <rebeccacaoliveira@gmail.com>
2023-01-02 18:48:20 -0300

    Update Inheritance.sol

--- Inheritance.sol
+++ Inheritance.sol
@@ -1,11 +1,12 @@
-//SPDX-License-Identifier: MIT
-
-pragma solidity ^0.8.8;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-contract LoveToken is ERC20 {
-    constructor(uint256 initialSupply) ERC20("Love", "LOV") {
-        _mint(msg.sender, 42069);
+contract Coffee is ERC20 {
+    constructor() ERC20("Coffee", "COF") {
+       _mint(msg.sender, 420690000000000000); 
     }
 }
+
+// https://goerli.etherscan.io/token/0xebb50880377d5ce1e316edc768463b4703f687d0?a=0x3DEe9017684E42056F03a19636D1Fd217Ce5b2A5 
