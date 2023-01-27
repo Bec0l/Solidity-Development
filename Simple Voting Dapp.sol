ac9edf9945a3904ff4ea8604fe0c9ac4f7397048
Bec0l <rebeccacaoliveira@gmail.com>
2023-01-27 17:21:52 -0300

    Update Simple Voting Dapp.sol

--- Simple Voting Dapp.sol
+++ Simple Voting Dapp.sol
@@ -1,59 +1,81 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.7;
 
-import "@openzeppelin/contracts/ownership/Ownable.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol";
 
 contract Voting is Ownable {
-    mapping (address => bool) public members;
-    mapping (uint256 => uint8) public votesReceived;
-    string[] public proposals;
-    bool public resultsShown;
 
-    struct Voted {
+    struct Proposals {
+        string proposal;
+        uint256 votesPerProposal;
+    }
+
+    Proposals[] public proposalForVoting;
+
+    struct Member {
+        address add;
         bool voted;
-    }
-    
-    event Results(uint8[] votes);
-    
-    function addMember(address member) public onlyOwner {
-        members[member] = true;
-    }
-
-    function addProposal(string memory proposal) public onlyOwner {
-        proposals.push(proposal);
-    }
-
-    function vote(uint256 proposal) public {
-        require(!resultsShown, "Voting closed.");
-        require(members[msg.sender], "You are not a member.");
-        require(proposals.length > 0, "No proposals available to vote for.");
-        require(proposal < proposals.length, "Invalid proposal.");
-        require(!voted[msg.sender], "You have already voted.");
-        votesReceived[proposal]++;
-        voted[msg.sender] = true;
-        voters.push(msg.sender);
-    }
-
-    function showResults() public returns (uint8[] memory) {
-        require(!resultsShown, "Results already shown.");
         
-        uint8[] memory result = new uint8[](proposals.length);
-        for (uint i = 0; i < proposals.length; i++) {
-            result[i] = votesReceived[i];
+    }
+
+    bool isMember;
+    
+    constructor () { 
+        for (uint i=0; i<member.length; i++){
+            if (member[i].add == msg.sender){
+                isMember = true;
+            } isMember = false;
         }
-        resultsShown = true;
+    }
+   
+    Member[] public member;
+
+    event Results (string, uint256 );
+
+    modifier memberOnly() {
+        require(isMember = true, "You are not a member");
+        _;
+    }
+
+    function addMember(address _address) public onlyOwner {
+        member.push(Member(_address, false));
         
-        emit Results(result);
+    }
+
+   function addProposal(string memory _proposal) public onlyOwner {
+        proposalForVoting.push(Proposals(_proposal,0));
+    } 
+
+// functio to add votes to the variable votesPerProposal. _porposalID is the position in the array
+    function vote(uint256 _proposalId) public memberOnly{
+        //require (!member(voted), "You have voted already.");
+        require(proposalForVoting.length > 0, "No proposals available to vote for.");
+        require(_proposalId < proposalForVoting.length, "Invalid proposal.");
+        proposalForVoting[_proposalId].votesPerProposal++;
+        
+        for (uint i=0; i<member.length; i++){
+            if (member[i].add == msg.sender){
+                member[i].voted = true;
+            }
+        }
+        
+    }
+
+    function showResults() public memberOnly {
+        for (uint i = 0; i < proposalForVoting.length; i++) {
+            emit Results (proposalForVoting[i].proposal, proposalForVoting[i].votesPerProposal);
+        }
     }
 
     function reset() public onlyOwner {
-        require(!voters[msg.sender].voted, "You have already voted.");
-        resultsShown = false;
-        voted = false;
-        while(proposals.length>0){
-            proposals.pop();
+        for (uint i = 0; i < member.length; i++) {
+            member[i].voted = false;
         }
-        
 
-    } 
+        while(proposalForVoting.length>0){
+            proposalForVoting.pop();
+        }
+
+    }
+
 }
